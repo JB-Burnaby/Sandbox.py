6d7c7504321c5ad0e80b7242397a296fce7edaf6
JB-Burnaby <jonah.burnaby@my.jcu.edu.au>
2017-03-17 09:16:25 +1000

    Add name print program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,8 @@
 ''' Jonah Burnaby '''
+
+name = input('Enter your name')
+
+if name.isspace()!= True:
+    print(name[1::2].replace(' ',''))
+else:
+    print('Name is not valid, please enter a string')
